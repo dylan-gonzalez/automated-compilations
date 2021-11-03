7c2bffb22dcad2d7201d6f1df69c96fa06301b75
dylan-gonzalez <dylcg10@gmail.com>
2021-11-03 21:08:54 +1100

    compilation support

--- movie.py
+++ movie.py
@@ -1,20 +1,20 @@
 # Import everything needed to edit video clips
 from moviepy.editor import *
 
-# Load myHolidays.mp4 and select the subclip 00:00:50 - 00:00:60
-clip = VideoFileClip("video.mp4").subclip(50,60)
+clips = []
+count = 0
+while True:
+    print(count)
+    try:
+        video = VideoFileClip("video" + str(count) + ".mp4").subclip(0,10) 
+        clips.append(video)
+    except:
+        break
 
-# Reduce the audio volume (volume x 0.8)
-clip = clip.volumex(0.8)
-
-# Generate a text clip. You can customize the font, color, etc.
-txt_clip = TextClip("My Holidays 2013",fontsize=70,color='white')
-
-# Say that you want it to appear 10s at the center of the screen
-txt_clip = txt_clip.set_pos('center').set_duration(10)
+    count += 1
 
 # Overlay the text clip on the first video clip
-video = CompositeVideoClip([clip, txt_clip])
+# video = CompositeVideoClip(clips)
 
-# Write the result to a file (many options available !)
-video.write_videofile("myHolidays_edited.webm")
+final_video = concatenate_videoclips(clips)
+final_video.write_videofile("compilation.webm")
--- tiktok.py
+++ tiktok.py
@@ -10,10 +10,15 @@ api = TikTokApi.get_instance()
 # need to pass it to methods you want to download
 device_id = api.generate_device_id()
 
-trending = api.by_trending(custom_device_id=device_id)
+tiktoks = api.by_trending(custom_device_id=device_id, count = 10)
+# tiktoks = api.by_hashtag("funny", count = 10, custom_device_id = device_id)
 
 # Below is if the method used if you have the full tiktok object
-video_bytes = api.get_video_by_tiktok(trending[0], custom_device_id=device_id)
+# video_bytes = api.get_video_by_tiktok(trending[0], custom_device_id=device_id)
 
-with open("video.mp4", "wb") as out:
-    out.write(video_bytes)
+# with open("video.mp4", "wb") as out:
+#     out.write(video_bytes)
+for count, video in enumerate(tiktoks):
+    file = open("video" + str(count) + ".mp4", "wb")
+    video_bytes =api.get_video_by_tiktok(tiktoks[count], custom_device_id=device_id) 
+    file.write(video_bytes)
