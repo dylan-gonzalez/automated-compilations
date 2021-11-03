c654d540203e4d9c1d0654d3ecf341890f0b27cb
dylan-gonzalez <dylcg10@gmail.com>
2021-11-03 18:53:00 +1100

    moviepy

--- webscraper.py
+++ webscraper.py
@@ -1,8 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
+import urllib3 as urllib
 
-URL = "https://realpython.github.io/fake-jobs/"
-page = requests.get(URL)
+
+
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36',
+}
+
+URL = "https://t.tiktok.com/"
+page = requests.get(URL, headers=headers)
+
+# print(page)
 
 soup = BeautifulSoup(page.content, "html.parser")
+# print(soup.prettify())
+videos = soup.find_all("video")
+body = soup.find("body")
+print(body.prettify())
+# print(soup.prettify())
+div = soup.find("div")
+# print(div)
+print(videos)
+
+
+# r1 = requests.get("https://www.tiktok.com/", headers=headers)
+# print(BeautifulSoup(r1.content, 'html.parser'))
+
+http = urllib.PoolManager()
+page2 = http.request("GET", "https://www.tiktok.com/", headers)
+soup = BeautifulSoup(page2.read())
 print(soup.prettify())
+
